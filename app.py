40c941ab8b2de42fe537ad2ae9761203ca4bfa56
Raelzin <69741130+Raelzin@users.noreply.github.com>
2021-02-04 13:24:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
 # 1 importar bibliotecas necessárias
+
 import pywhatkit
 import keyboard
 import time
 from datetime import datetime
-# 2 definir contatos
-contatos = ['+5585996862109']
+
+# 2 definir contatos, necessário colocar o número neste formato: +5585912345678
+
+contatos = ['']
+
 # 3 intervalo de envio p/ cada contato
+
 while len(contatos) >= 1:
     # enviar mensagens
+    
     pywhatkit.sendwhatmsg(contatos[0], 'Testando bot de whatsapp', datetime.now().hour,datetime.now().minute + 2)
     del contatos[0]
     time.sleep(15)
     keyboard.press_and_release('ctrl + w')
-
-# 4 testar
